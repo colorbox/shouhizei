163b30ed0e13aa435a7a72e672e8ca0983d0f0bd
colorbox <colorbox222@gmail.com>
2017-08-01 01:22:16 +0900

    Add core logic and test

--- lib/shouhizei.rb
+++ lib/shouhizei.rb
@@ -1,5 +1,18 @@
 require "shouhizei/version"
 
+require'yaml'
+
 module Shouhizei
-  # Your code goes here...
+  def self.rate_on(date = Date.today)
+    rate_list.reverse_each do |key_date, rate|
+      return rate.to_r if date >= key_date
+    end
+    0.0r
+  end
+
+  private
+
+  def self.rate_list
+    @@rate_list ||= YAML.load_file(File.expand_path('./../rate_list.yml', __FILE__))
+  end
 end
--- test/shouhizei_test.rb
+++ test/shouhizei_test.rb
@@ -1,11 +1,13 @@
 require "test_helper"
 
 class ShouhizeiTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Shouhizei::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+  def test_rate_on
+    assert_equal(0.0r,  Shouhizei.rate_on(Date.new(1989, 3, 31)))
+    assert_equal(0.03r, Shouhizei.rate_on(Date.new(1989, 4, 1)))
+    assert_equal(0.03r, Shouhizei.rate_on(Date.new(1997, 3, 31)))
+    assert_equal(0.05r, Shouhizei.rate_on(Date.new(1997, 4, 1)))
+    assert_equal(0.05r, Shouhizei.rate_on(Date.new(2014, 3, 31)))
+    assert_equal(0.08r, Shouhizei.rate_on(Date.new(2014, 4, 1)))
+    assert_equal(0.08r, Shouhizei.rate_on())
   end
 end
