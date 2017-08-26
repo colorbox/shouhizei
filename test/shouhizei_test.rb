5ce3a1870206deda32ee241a0e7c55843acd8359
colorbox <colorbox222@gmail.com>
2017-08-27 03:03:27 +0900

    Add price calculation
      - add calculation method
      - add configuration method for rounding
      - add test

--- test/shouhizei_test.rb
+++ test/shouhizei_test.rb
@@ -1,4 +1,5 @@
-require "test_helper"
+require 'test_helper'
+require 'timecop'
 
 class ShouhizeiTest < Minitest::Test
   def test_rate_on
@@ -9,5 +10,21 @@ class ShouhizeiTest < Minitest::Test
     assert_equal(0.05r, Shouhizei.rate_on(Date.new(2014, 3, 31)))
     assert_equal(0.08r, Shouhizei.rate_on(Date.new(2014, 4, 1)))
     assert_equal(0.08r, Shouhizei.rate_on())
+  end
+
+  def test_price_calculation
+    assert_equal(108, Shouhizei.included(price: 100, date: Date.new(2014, 4, 1)))
+
+    Timecop.freeze(Date.new(2014, 4, 1)) do
+      Shouhizei.config[:rounding] = Shouhizei::RoundUp
+      round_up_price = Shouhizei.included(price: 10)
+      assert_equal(11, round_up_price)
+      assert_instance_of(Integer, round_up_price)
+
+      Shouhizei.config[:rounding] = Shouhizei::RoundDown
+      round_down_price = Shouhizei.included(price: 10)
+      assert_equal(10, round_down_price)
+      assert_instance_of(Integer, round_down_price)
+    end
   end
 end
